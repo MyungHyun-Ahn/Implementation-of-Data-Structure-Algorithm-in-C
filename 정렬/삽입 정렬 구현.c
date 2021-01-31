f16437e2ae7a2d1f627a616295c0000d95057a43
MyungHyun-Ahn <ahn020367@gmail.com>
2021-01-31 20:17:42 +0900

    Implementing Insertion Sort

--- 정렬/삽입 정렬 구현.c
+++ 정렬/삽입 정렬 구현.c
@@ -1,34 +1,30 @@
 //배열 선언
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <limits.h>
 #define SIZE 1000
 
 int a[SIZE];
 
-//스왑 함수 : 정렬은 두개의 위치를 바꾸는 일이 많음.
+//스왑 함수
 int swap(int *a, int *b) {
 	int temp = *a;
 	*a = *b;
 	*b = temp;
 }
 
-//선택 정렬 수행
+//삽입 정렬 수행
 int main(void) {
-	int n, min, index;
+	int n;
 	scanf("%d", &n);
 	for (int i = 0;i < n;i++) {
 		scanf("%d", &a[i]);
 	}
-	for (int i = 0;i < n;i++) {
-		min = INT_MAX;
-		for (int j = i;j < n;j++) {
-			if (min > a[j]) {
-				min = a[j];
-				index = j;
-			}
+	for (int i = 0;i < n - 1;i++) {
+		int j = i;
+		while (j >= 0 && a[j] > a[j + 1]) {
+			swap(&a[j], &a[j + 1]);
+			j--;
 		}
-		swap(&a[i], &a[index]);
 	}
 	for (int i = 0;i < n;i++) {
 		printf("%d ", a[i]);
